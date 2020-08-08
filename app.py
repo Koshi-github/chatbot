0854b7cdde06c8caad9fd19389fcca653beaf797
koshi <koshiyamashita@koushiyamashita-no-MacBook-Air.local>
2020-08-09 00:16:15 +0900

    make it better

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_message(event):
         nyanMessage = "数当てゲームを終了するニャン！"
         numberGameFlag = False
     elif message == "天気":
-        nyanMessage = "今の岐阜の天気は" + getTenki() + "ニャン!”
+        nyanMessage = "今の岐阜の天気は" + getTenki() + "ニャン!"
 
     line_bot_api.reply_message(
         event.reply_token,
