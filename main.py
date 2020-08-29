6f9dbe7f47280a2a9a43a4678291ae5b9b1946ad
山下 航志 <genius_eternia@yahoo.co.jp>
2020-08-29 17:57:43 +0900

    make it better

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ import commonEnum
 def execute(message, userID):
     returnMessage = []
 
-    modeController = ModeController(userID)
-    mode = modeController.getMode()
-
-    if("食材" in message):
-        modeController.setMode(commonEnum.Mode.SYOKUZAI)
-        returnMessage.append("何の材料が知りたいニャン？")
-        return returnMessage
-    
-    elif("クラシル" in message):
-        modeController.setMode(commonEnum.Mode.KURASHIRU)
-        returnMessage.append("何の料理を検索するニャン？")
-        return returnMessage
-
     try:
+        modeController = ModeController(userID)
+        mode = modeController.getMode()
+
+        if("食材" in message):
+            modeController.setMode(commonEnum.Mode.SYOKUZAI)
+            returnMessage.append("何の材料が知りたいニャン？")
+            return returnMessage
+        
+        elif("クラシル" in message):
+            modeController.setMode(commonEnum.Mode.KURASHIRU)
+            returnMessage.append("何の料理を検索するニャン？")
+            return returnMessage
+
+    
 
         if ("天気" in message):
             tenki = Tenki()
