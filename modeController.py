ff11fd3e6ca86be336253fcf570dc73f017e1eea
山下 航志 <genius_eternia@yahoo.co.jp>
2020-08-29 18:35:12 +0900

    make it better

--- modeController.py
+++ modeController.py
@@ -22,7 +22,7 @@ class ModeController:
     def setMode(self,mode):
         targetRow = self.sp.GetDataRow(self.userID)
         if targetRow > 0:
-            self.sp.Write(targetRow,2,commonEnum.Mode[mode])
+            self.sp.Write(targetRow,2,mode.value)
 
 
 
