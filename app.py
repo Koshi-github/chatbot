905b88a63d5e82503edb27997838696a2811f18f
山下 航志 <genius_eternia@yahoo.co.jp>
2020-08-29 18:31:03 +0900

    make it better

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    returnMessage = main.execute(event.message.text, line_bot_api.get_profile(event.source.user_id))
+    returnMessage = main.execute(event.message.text, str(line_bot_api.get_profile(event.source.user_id).user_id))
     sendMessage(event,returnMessage)
 
 if __name__ == "__main__":
